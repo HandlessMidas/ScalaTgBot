52cdb74268a544905b3c122598d91afeedafa428
Gleb Oborin <oborin.gleb@yandex.ru>
2020-03-12 15:31:45 +0300

    Old but gold

--- src/main/scala/bot/BotStarter.scala
+++ src/main/scala/bot/BotStarter.scala
@@ -23,14 +23,14 @@ class BotStarter(override val client: RequestHandler[Future]) extends TelegramBo
   with Commands[Future] {
 
   val registeredUsers: mutable.Set[User] = mutable.Set[User]()
+  var messagesUsers: mutable.Map[String, mutable.ListBuffer[(String, String)]] = mutable.Map[String, mutable.ListBuffer[(String, String)]]().withDefaultValue(mutable.ListBuffer())
 
   onCommand("/start") { implicit msg =>
     msg.from match {
       case None => reply("Register error").void
-      case Some(user) => {
-          registeredUsers += user
-          reply(s"You're registered.\n Your id is ${user.id}").void
-      }
+      case Some(user) =>
+        registeredUsers += user
+        reply(s"You're registered.\nYour id is ${user.id}").void
     }
   }
 
@@ -40,6 +40,37 @@ class BotStarter(override val client: RequestHandler[Future]) extends TelegramBo
       it => usersString += s"${it.firstName} ${it.lastName}\n"
     }
     reply(usersString).void
+  }
+
+  onCommand("/check") { implicit msg =>
+    msg.from match {
+      case None => reply("Error.").void
+      case Some(user) =>
+        messagesUsers(user.id.toString).foreach { p =>
+          reply(s"From: ${p._1}\n${p._2}\n\n")
+        }
+        reply("").void
+    }
+  }
+
+  onCommand("/send") { implicit msg =>
+    var from_id : Int = 0
+    msg.from match {
+      case None => from_id = 0
+      case Some(user) => from_id = user.id
+    }
+
+    msg.text match {
+      case None => reply("Enter non-empty message").void
+      case Some(s) =>
+        val id: String = s.slice(6, 15)
+        val text: String = s.slice(16, s.length)
+        if (!messagesUsers.contains(id)) {
+          messagesUsers(id) = mutable.ListBuffer()
+        }
+        messagesUsers(id) += Tuple2(from_id.toString, text)
+        reply(s"Message was sent to ${id}").void
+    }
   }
 }
 
