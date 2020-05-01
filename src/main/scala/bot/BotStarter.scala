f5d51f60026803026431db082af8fc40b2d0cbbf
handlessmidas <midas1024@yandex.ru>
2020-05-01 15:38:47 +0500

    Divided into classes

--- src/main/scala/bot/BotStarter.scala
+++ src/main/scala/bot/BotStarter.scala
@@ -1,6 +1,5 @@
 package bot
 
-import bot.BotStarter.Service
 import cats.instances.future._
 import cats.syntax.functor._
 import com.bot4s.telegram.api.RequestHandler
@@ -23,67 +22,73 @@ import scala.util.Random
 import scala.io.Source
 import com.softwaremill.sttp._
 import com.softwaremill.sttp.json4s._
+import org.json4s.native.Serialization
 
-class BotStarter(override val client: RequestHandler[Future], val service: Service) extends TelegramBot
+class BotStarter(override val client: RequestHandler[Future], val service: Service,
+                 val userHandler: UserHandler, val messageHandler: MessageHandler) extends TelegramBot
   with Polling
   with Commands[Future] {
-
-  val registeredUsers: mutable.Set[User] = mutable.Set[User]()
 
   onCommand("/start") { implicit msg =>
     msg.from match {
       case None => reply("Register error").void
-      case Some(user) => {
-          registeredUsers += user
-          reply(s"You're registered.\nYour id is ${user.id}").void
-      }
+      case Some(user) =>
+        userHandler.register(user)
+        reply(s"You're registered.\nYour id is ${user.id}").void
     }
   }
 
   onCommand("/users") { implicit msg =>
-    var usersString = ""
-    registeredUsers.foreach {
-      it => usersString += s"${it.firstName} ${it.lastName}\n"
+    reply(userHandler.show()).void
+  }
+
+  onCommand("/check") { implicit msg =>
+    msg.from match {
+      case None => reply("Error.").void
+      case Some(user) =>
+        val messages = messageHandler.show(user.id.toString)
+        messageHandler.clear(user.id.toString)
+        reply(messages).void
     }
-    reply(usersString).void
+  }
+
+  onCommand("/send") { implicit msg =>
+    var from_id : Int = 0
+    msg.from match {
+      case None => from_id = 0
+      case Some(user) => from_id = user.id
+    }
+
+    msg.text match {
+      case None => reply("Enter non-empty message").void
+      case Some(s) =>
+        val id: String = s.slice(6, 15)
+        val text: String = s.slice(16, s.length)
+        messageHandler.send(from_id.toString, id, text)
+        reply(s"Message was sent to $id").void
+    }
   }
 
   onCommand("/cat") { implicit msg =>
-    service.getCat().flatMap(reply(_)).void
+    service.getCat.flatMap(reply(_)).void
   }
 }
 
 object BotStarter {
-
-  implicit val serialization =  org.json4s.native.Serialization
-
-  case class Response(data: List[Data])
-  case class Data(link: String)
-
-  class Service(implicit val backend: SttpBackend[Future, Nothing]) {
-    implicit val ec: ExecutionContext = ExecutionContext.global
-    val request: RequestT[Id, Response, Nothing] = sttp
-      .header("Authorization", "Client-ID 2a47c24862afdf7")
-      .get(uri"https://api.imgur.com/3/gallery/search?q=cats")
-      .response(asJson[Response])
-
-    def getCat() = backend.send(request).map { response =>
-      scala.util.Random.shuffle(response.unsafeBody.data).head.link
-    }
-  }
-
   def main(args: Array[String]): Unit = {
     implicit val ec: ExecutionContext = ExecutionContext.global
     implicit val backend: SttpBackend[Future, Nothing] = OkHttpFutureBackend(
       SttpBackendOptions.Default.socksProxy("ps8yglk.ddns.net", 11999)
     )
 
+    val userHandler = new UserHandler
+    val messageHandler = new MessageHandler
     val fileSource = Source.fromFile("token.txt")
     val token = fileSource.mkString
     fileSource.close()
 
     val service: Service = new Service()
-    val bot = new BotStarter(new FutureSttpClient(token), service)
+    val bot = new BotStarter(new FutureSttpClient(token), service, userHandler, messageHandler)
     Await.result(bot.run(), Duration.Inf)
   }
 }
