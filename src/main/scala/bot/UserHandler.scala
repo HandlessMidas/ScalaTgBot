56107608e2ef328094c25a2541ebd7dda057f912
Gleb Oborin <oborin.gleb@yandex.ru>
2020-05-08 21:21:16 +0300

    refactored UserHandler

--- src/main/scala/bot/UserHandler.scala
+++ src/main/scala/bot/UserHandler.scala
@@ -4,12 +4,15 @@ import com.bot4s.telegram.models.User
 
 import scala.collection.mutable
 
+
 class UserHandler {
+  def getUserDescription(user : User) = s"${user.firstName} ${user.lastName.getOrElse("")}, id: ${user.id}\n"
+
   val registeredUsers: mutable.Set[User] = mutable.Set[User]()
 
   def register(user: User): Unit = registeredUsers += user
 
   def show(): String = registeredUsers.foldLeft("") {
-    (last, user) => last + s"${user.firstName} ${user.lastName.getOrElse("")}, id: ${user.id}\n"
+    (last, user) => last + getUserDescription(user)
   }
 }
---
+++ src/test/scala/bot/MessageHandlerTest.scala
@@ -0,0 +1,9 @@
+package bot
+
+import org.scalamock.scalatest.MockFactory
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.must.Matchers
+
+class MessageHandler extends AnyFlatSpec with Matchers with MockFactory {
+
+}
